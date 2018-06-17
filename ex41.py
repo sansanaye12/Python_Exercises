86efb3135f371e8c076bf8773e9620d9bda69211
sansanaye12 <eipo0858@gmail.com>
2018-06-17 11:05:03 +0630

    add

--- ex41.py
+++ ex41.py
@@ -4,18 +4,19 @@ import sys
 WORD_URL="http://learncodethehardway.org/words.txt"
 WORDS=[]
 PHRASES={
-    "class %%%(%%%):":
-        "Make a class named %%% that is_a %%%.",
-    "class %%%(object):\n\tdef ___init___(self, ***)":
-        "class %%% has-a ___init___ that takes self and *** params.",
-    "class %%%(object):\n\tdef ***(self,@@@)":
-        "class %%% has-a function *** that takes self and @@@ params.",
-    "***=%%%()":
-        "Set *** to an  instance of class %%%.",
-    "***.***(@@@)":
-        "From *** get the *** function, call it with params self, @@@.",
-    "***.***='***":
-        "From *** get the *** attribute and set it to '***'."
+        "class %%%(%%%):":
+         "Make a class named %%% that is_a %%%.",
+        "class %%%(object):\n\tdef ___init___(self, ***)":
+         "class %%% has-a ___init___ that takes self and *** params.",
+        "class %%%(object):\n\tdef ***(self,@@@)":
+         "class %%% has-a function *** that takes self and @@@ params.",
+        "***=%%%()":
+         "Set *** to an  instance of class %%%.",
+        "***.***(@@@)":
+         "From *** get the *** function, call it with params self, @@@.",
+        "***.***='***":
+         "From *** get the *** attribute and set it to '***'."
+         }
 if len(sys.argv) == 2 and sys.argv[1] == "english":
     PHRASE_FIRST  = True
 else:
@@ -29,7 +30,7 @@ def convert(snippet,phrase):
     param_names=[]
     for i in range(0, snippet.count("@@@")):
         param_count=random.randint(1,3)
-        param_names.append(', '.join(random.sample('WORDS,param_count)))
+        param_names.append(', '.join(random.sample(WORDS,param_count)))
     for sentence in snippet, phrase:
         result=sentence[:]
         for word in class_names:
